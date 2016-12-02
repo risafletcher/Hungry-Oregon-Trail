354b160d678a3f3e34539aca79a67c73d01233c6
Michael Freeman <mjfreem1@gmail.com>
2016-12-01 18:16:39 -0800

    finished sharing functions

--- build/bundle.js
+++ build/bundle.js
@@ -32541,15 +32541,16 @@
 	
 	function sharingController($scope) {
 	    $scope.fate = '';
-	    $scope.percentage = 60;
 	
-	    if ($scope.percentage <= 10) {
-	        $scope.fate = 'The wagon\'s occupants accuse you of miserliness and kill you.';
-	    } else if ($scope.percentage > 10 && $scope.percentage <= 50) {
-	        $scope.fate = 'The wagon\'s occupants don\'t want another mouth to feed, so they kill you.';
-	    } else {
-	        $scope.fate = 'You were too generous. Your food eventually spoiled and you all died.';
-	    }
+	    $scope.submit = function () {
+	        if ($scope.percentage <= 10) {
+	            $scope.fate = 'The wagon\'s occupants accuse you of miserliness and kill you.';
+	        } else if ($scope.percentage > 10 && $scope.percentage <= 50) {
+	            $scope.fate = 'The wagon\'s occupants don\'t want another mouth to feed, so they kill you.';
+	        } else {
+	            $scope.fate = 'You were too generous. Your food eventually spoiled and you all died.';
+	        }
+	    };
 	}
 
 /***/ }
--- src/controllers/sharing.js
+++ src/controllers/sharing.js
@@ -1,15 +1,26 @@
 sharingController.$inject = ['$scope'];
 
 export default function sharingController($scope) {
-    $scope.fate = '';
   
-  $scope.submit = function () {
+  $scope.submitWagon = function () {
+    $scope.fate = '';
     if($scope.percentage <= 10) {
         $scope.fate = 'The wagon\'s occupants accuse you of miserliness and kill you.';
     } else if($scope.percentage > 10 && $scope.percentage <= 50) {
         $scope.fate = 'The wagon\'s occupants don\'t want another mouth to feed, so they kill you.';
     } else {
         $scope.fate = 'You were too generous. Your food eventually spoiled and you all died.';
+    }
+  }
+
+  $scope.submitIndian = function () {
+    $scope.fate = '';
+    if($scope.percentage <= 10) {
+        $scope.fate = 'The Indians accuse you of miserliness and kill you.';
+    } else if($scope.percentage > 10 && $scope.percentage <= 50) {
+        $scope.fate = 'The tribe decides you aren\'t worth it. They take your food and leave you to die.';
+    } else {
+        $scope.fate = 'The Indians respect your generosity and take you. They eventually help you to Oregon.';
     }
   }
 }
