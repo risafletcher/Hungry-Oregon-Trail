ce3ff64bcc555fc64a0ddc06bb373767e81e1a5b
Michael Freeman <mjfreem1@gmail.com>
2016-12-01 17:01:16 -0800

    added food functions

--- src/controllers/food.js
+++ src/controllers/food.js
@@ -2,11 +2,17 @@ foodController.$inject = ['$scope'];
 
 export default function foodController($scope) {
     $scope.foodLevel = 100;
-    $scope.food = function() {
+    
+    $scope.eatFood = function() {
         $scope.foodLevel = $scope.foodLevel - (Math.floor(Math.random() * (20 - 11)) + 10);
-        // $scope.foodLevel = $scope.foodLevel - Math.floor(Math.random() * 20);
     };
+
+    $scope.loseFood = function() {
+        $scope.foodLevel = scope.foodLevel * 0;
+    };
+
+    $scope.addFood = function() {
+        $scope.foodLevel = $scope.foodLevel + (Math.floor(Math.random() * (20 - 11)) + 10);
+    ;}
   
-
-
 }
