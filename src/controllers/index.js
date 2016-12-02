5dbcc461a018c1fae3d58449b7503b290ab79a00
Michael Freeman <mjfreem1@gmail.com>
2016-12-01 17:25:41 -0800

    food sharing functions

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 import angular  from 'angular';
 import food from './food';
+import sharing from './sharing';
 
 //controllers will go into this 'bucket'
 const controllers = angular.module('controllers',[]);
 
 controllers.controller('food', food);
+controllers.controller('sharing', sharing)
 
 export default controllers.name;
