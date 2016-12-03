fbb354efdd183a44ccd9215b02545d8f041c9eab
Nathan Hugon <nthugon@gmail.com>
2016-12-02 18:09:50 -0800

    game restarts successfully

--- build/bundle.js
+++ build/bundle.js
@@ -32528,6 +32528,33 @@
 	
 	    $scope.restart = function () {
 	        $scope.foodLevel = 100;
+	        $scope.beginGame = false;
+	    };
+	
+	    // $scope.offer = function() {
+	    //     $scope.choice5 = true;
+	    // };
+	
+	    $scope.submitWagon = function () {
+	        $scope.fate = '';
+	        if ($scope.percentage <= 10) {
+	            $scope.fate = 'The wagon\'s occupants accuse you of miserliness and kill you.';
+	        } else if ($scope.percentage > 10 && $scope.percentage <= 50) {
+	            $scope.fate = 'The wagon\'s occupants don\'t want another mouth to feed, so they kill you.';
+	        } else {
+	            $scope.fate = 'You were too generous. Your food eventually spoiled and you all died.';
+	        }
+	    };
+	
+	    $scope.submitIndian = function () {
+	        $scope.fate = '';
+	        if ($scope.percentage <= 10) {
+	            $scope.fate = 'The Indians accuse you of miserliness and kill you slowly.';
+	        } else if ($scope.percentage > 10 && $scope.percentage <= 50) {
+	            $scope.fate = 'The tribe decides you aren\'t worth it. They take your food and leave you to die.';
+	        } else {
+	            $scope.fate = 'The Indians respect your generosity and take you. They eventually help you to Oregon. YOU WIN!';
+	        }
 	    };
 	}
 
@@ -32563,7 +32590,7 @@
 	        } else if ($scope.percentage > 10 && $scope.percentage <= 50) {
 	            $scope.fate = 'The tribe decides you aren\'t worth it. They take your food and leave you to die.';
 	        } else {
-	            $scope.fate = 'The Indians respect your generosity and take you. They eventually help you to Oregon.';
+	            $scope.fate = 'The Indians respect your generosity and take you. They eventually help you to Oregon. YOU WIN!';
 	        }
 	    };
 	}
--- src/controllers/food.js
+++ src/controllers/food.js
@@ -17,6 +17,33 @@ export default function foodController($scope) {
 
     $scope.restart = function() {
         $scope.foodLevel = 100;
+        $scope.beginGame = false;
     };
-  
+
+    // $scope.offer = function() {
+    //     $scope.choice5 = true;
+    // };
+
+    $scope.submitWagon = function () {
+        $scope.fate = '';
+        if($scope.percentage <= 10) {
+            $scope.fate = 'The wagon\'s occupants accuse you of miserliness and kill you.';
+        } else if($scope.percentage > 10 && $scope.percentage <= 50) {
+            $scope.fate = 'The wagon\'s occupants don\'t want another mouth to feed, so they kill you.';
+        } else {
+            $scope.fate = 'You were too generous. Your food eventually spoiled and you all died.';
+        }
+    };
+
+    $scope.submitIndian = function () {
+        $scope.fate = '';
+        if($scope.percentage <= 10) {
+            $scope.fate = 'The Indians accuse you of miserliness and kill you slowly.';
+        } else if($scope.percentage > 10 && $scope.percentage <= 50) {
+            $scope.fate = 'The tribe decides you aren\'t worth it. They take your food and leave you to die.';
+        } else {
+            $scope.fate = 'The Indians respect your generosity and take you. They eventually help you to Oregon. YOU WIN!';
+        }
+    };
+ 
 }
